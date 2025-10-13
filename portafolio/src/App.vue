4db95d7fc8070627326d047a8303a82c87d6464e
SergioAndresG <sergiogarcia3421gmail.com>
2025-10-13 11:14:48 -0500

    chore(): creacion del proyecto

---
+++ portafolio/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <h1>Mi Portafolio</h1>
+  </header>
+  <main>
+    <!-- El contenido de tus páginas irá aquí -->
+  </main>
+</template>
+
+<style scoped>
+/* Puedes empezar tus estilos aquí */
+h1 {
+  text-align: center;
+}
+</style>

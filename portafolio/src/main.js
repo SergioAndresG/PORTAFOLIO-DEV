bf4e6d4ea2441278fc1a284a515d2e9a87e34ba3
SergioAndresG <140452759+SergioAndresG@users.noreply.github.com>
2025-10-17 22:32:30 -0500

    feat: Integrar Font Awesome y registrar iconos en la aplicación

--- portafolio/src/main.js
+++ portafolio/src/main.js
@@ -3,6 +3,18 @@ import './style.css'
 import App from './App.vue'
 import router from './routes/myroutes'
 
+// componentes base para Fonts-Awesome
+import { library } from '@fortawesome/fontawesome-svg-core' 
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// importamos lo estilos
+import { faLinkedin, faInstagram, faGithub } from '@fortawesome/free-brands-svg-icons'
+
+// añadimos los iconos importados a la libreria
+library.add(faLinkedin, faInstagram, faGithub)
+
 createApp(App)
+// registramos el componente global
+.component('font-awesome-icon', FontAwesomeIcon)
 .use(router)
 .mount('#app')

95ef6b51d3787a7c5e67abde271b501a747f208b
SergioAndresG <140452759+SergioAndresG@users.noreply.github.com>
2025-11-05 01:22:12 -0500

    feat(Achievements.vue): Se agrega la card, y animación correspondiente a la sección

--- portafolio/src/components/Achievements.vue
+++ portafolio/src/components/Achievements.vue
@@ -60,7 +60,23 @@ onUnmounted(() => {
             </p>
         </div>
 
-
+    <div class="container">
+        <div class="achievements-card">
+            <div class="container-trophy">
+            <svg class="svg-icon" height="100" preserveAspectRatio="xMidYMid meet" viewBox="0 0 100 100" width="100" x="0" xmlns="http://www.w3.org/2000/svg" y="0">
+                <path d="M62.11,53.93c22.582-3.125,22.304-23.471,18.152-29.929-4.166-6.444-10.36-2.153-10.36-2.153v-4.166H30.099v4.166s-6.194-4.291-10.36,2.153c-4.152,6.458-4.43,26.804,18.152,29.929l5.236,7.777v8.249s-.944,4.597-4.833,4.986c-3.903,.389-7.791,4.028-7.791,7.374h38.997c0-3.347-3.889-6.986-7.791-7.374-3.889-.389-4.833-4.986-4.833-4.986v-8.249l5.236-7.777Zm7.388-24.818s2.833-3.097,5.111-1.347c2.292,1.75,2.292,15.86-8.999,18.138l3.889-16.791Zm-44.108-1.347c2.278-1.75,5.111,1.347,5.111,1.347l3.889,16.791c-11.291-2.278-11.291-16.388-8.999-18.138Z">
+                </path>
+            </svg>  
+            <div class="container__star">
+                
+                <div class="star-eight"></div>
+            </div>
+            
+            <div></div></div>
+            <h3 class="title-card">SENA Soft 2024</h3>
+            <p class="subtitle-card">Gracias a la divulagacion de la competencia, puede presentar un cuestionario para participar en la categoria de desarrollo integral, en el cual tuve un resuktado favorable y cumpli requisitos para poarticipar en la categoria, dentro de la misma, forme equipo con dos personas maravillosas, con quienes nos adaptamos para desarrollar la solución que teniamos plateada, fue una competencia con duración de una semana, en donde implemetamos todas nuestras habilidades, como desarrolladores, luego, fuimos a la sede de globant en Cali, valle del cauca en donde presentamos nuestra solución al jurado de la empresa, gracias a nuestros conocimientos, esfuerzo y dedicación, ocupamos el primer lugar en la categoria</p>
+        </div>
+    </div>
 
     </section>
 
@@ -94,17 +110,129 @@ onUnmounted(() => {
   opacity: 1;
   transform: translateY(0);
 }
-
 .title {
   font-size: 3rem;
   font-weight: 700;
   color: white;
   margin-bottom: 1rem;
 }
-
 .subtitle {
   font-size: 1.25rem;
   color: #cbd5e1;
   transition-delay: 150ms;
+}
+.container { 
+   width: 800px;
+   margin: 0 auto;
+}
+.achievements-card {
+  position: relative;
+  border: 2px solid #475569;
+  background: rgba(30, 41, 59, 0.5);
+  backdrop-filter: blur(10px);
+  border-radius: 1rem;
+  padding: 1.25rem;
+  text-align: center;
+}
+.title-card{
+    color: white;
+    font-size: 1.5rem;
+    padding-bottom: 1.5rem;
+    border-bottom: 1px solid #475569;
+}
+.subtitle-card{
+    color: rgb(187, 187, 187);
+}
+
+/* From Uiverse.io by Javierrocadev */ 
+.container-trophy {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: 5em;
+  font-weight: 900;
+  color: #e10600;
+  position: relative;
+  transition: all 1s ease;
+  text-align: center;
+}
+
+.container__star {
+  transition: all .7s ease-in-out;
+}
+
+.first {
+  position: absolute;
+  top: 20px;
+  left: 50px;
+  transition: all .7s ease-in-out;
+}
+
+.svg-icon {
+  position: absolute;
+  fill: #e94822;
+  z-index: 1;
+}
+
+.star-eight {
+  background: #efd510;
+  width: 150px;
+  height: 150px;
+  position: relative;
+  text-align: center;
+  animation: rot 3s  infinite;
+}
+
+.star-eight::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 150px;
+  width: 150px;
+  background: #efd510;
+  transform: rotate(135deg);
+}
+
+.container-trophy:hover .container__star {
+  transform: rotateX(70deg) translateY(250px);
+  box-shadow: 0px 0px 120px -100px #e4e727;
+}
+
+.container-trophy:hover .svg-icon {
+  animation: grow 1s linear infinite;
+}
+
+@keyframes rot {
+  0% {
+    transform: rotate(0deg);
+  }
+
+  50% {
+    transform: rotate(340deg);
+  }
+
+  100% {
+    transform: rotate(0deg);
+  }
+}
+
+@keyframes grow {
+  0% {
+    transform: rotate(0deg);
+  }
+
+  25% {
+    transform: rotate(-5deg);
+  }
+
+  75% {
+    transform: rotate(5deg);
+  }
+
+  100% {
+    transform: scale(1) rotate(0deg);
+  }
 }
 </style>

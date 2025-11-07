7f9de28e60ba5b6597485ea4a8280095a74cd62a
SergioAndresG <140452759+SergioAndresG@users.noreply.github.com>
2025-11-07 13:46:30 -0500

    refactor(Achievements.vue): indentación del codigo

--- portafolio/src/components/Achievements.vue
+++ portafolio/src/components/Achievements.vue
@@ -66,26 +66,26 @@ onUnmounted(() => {
             <p class="subtitle">Logros en mi carrera como Dev</p>
             </div>
             <div class="achievement-card">
-            <!-- Header con icono e info -->
-            <div class="achievement-header">
-                <div class="icon-wrapper">
-                </div>
-                <div class="container-trophy">
-                    <svg class="svg-icon" height="40" preserveAspectRatio="xMidYMid meet" viewBox="0 0 100 100" width="100" x="0" xmlns="http://www.w3.org/2000/svg" y="0">
-                        <path d="M62.11,53.93c22.582-3.125,22.304-23.471,18.152-29.929-4.166-6.444-10.36-2.153-10.36-2.153v-4.166H30.099v4.166s-6.194-4.291-10.36,2.153c-4.152,6.458-4.43,26.804,18.152,29.929l5.236,7.777v8.249s-.944,4.597-4.833,4.986c-3.903,.389-7.791,4.028-7.791,7.374h38.997c0-3.347-3.889-6.986-7.791-7.374-3.889-.389-4.833-4.986-4.833-4.986v-8.249l5.236-7.777Zm7.388-24.818s2.833-3.097,5.111-1.347c2.292,1.75,2.292,15.86-8.999,18.138l3.889-16.791Zm-44.108-1.347c2.278-1.75,5.111,1.347,5.111,1.347l3.889,16.791c-11.291-2.278-11.291-16.388-8.999-18.138Z">
-                        </path>
-                    </svg>  
-                    <div class="container__star">
-                        
-                        <div class="star-eight"></div>
+                <!-- Header con icono e info -->
+                <div class="achievement-header">
+                    <div class="icon-wrapper">
                     </div>
-                
-                <div></div></div>
-                <div class="header-content">
-                <h3 class="achievement-title">SENA Soft 2024</h3>
-                <div class="achievement-meta">
-                    <span class="meta-tag category">Desarrollo Integral</span>
-                </div>
+                    <div class="container-trophy">
+                        <svg class="svg-icon" height="40" preserveAspectRatio="xMidYMid meet" viewBox="0 0 100 100" width="100" x="0" xmlns="http://www.w3.org/2000/svg" y="0">
+                            <path d="M62.11,53.93c22.582-3.125,22.304-23.471,18.152-29.929-4.166-6.444-10.36-2.153-10.36-2.153v-4.166H30.099v4.166s-6.194-4.291-10.36,2.153c-4.152,6.458-4.43,26.804,18.152,29.929l5.236,7.777v8.249s-.944,4.597-4.833,4.986c-3.903,.389-7.791,4.028-7.791,7.374h38.997c0-3.347-3.889-6.986-7.791-7.374-3.889-.389-4.833-4.986-4.833-4.986v-8.249l5.236-7.777Zm7.388-24.818s2.833-3.097,5.111-1.347c2.292,1.75,2.292,15.86-8.999,18.138l3.889-16.791Zm-44.108-1.347c2.278-1.75,5.111,1.347,5.111,1.347l3.889,16.791c-11.291-2.278-11.291-16.388-8.999-18.138Z">
+                            </path>
+                        </svg>  
+                        <div class="container__star">
+                            
+                            <div class="star-eight"></div>
+                        </div>
+                    
+                    <div></div></div>
+                    <div class="header-content">
+                    <h3 class="achievement-title">SENA Soft 2024</h3>
+                    <div class="achievement-meta">
+                        <span class="meta-tag category">Desarrollo Integral</span>
+                    </div>
                 </div>
             </div>
 

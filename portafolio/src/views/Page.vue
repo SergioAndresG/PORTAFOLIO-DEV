f68a950fca9ba4f3c9f05c6abad302aa680c0c34
SergioAndresG <140452759+SergioAndresG@users.noreply.github.com>
2025-11-12 17:27:51 -0500

    feat(Page.vue): Componente donde se veran todos los componentes

---
+++ portafolio/src/views/Page.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import Header from '../components/Header.vue'
+import AboutMe from '../components/AboutMe.vue'
+import StackSection from '../components/StackSection.vue'
+import ProyectsSection from '../components/ProyectSection.vue'
+import WorkExperience from '../components/WorkExperience.vue'
+import Achievements from '../components/Achievements.vue'
+import ContactMe from '../components/ContactMe.vue'
+import Home from '../components/Home.vue'
+</script>
+
+<template>
+  <Header></Header>
+  <Home />
+  <AboutMe  id="about-me" />
+  <StackSection id="stack"/>
+  <ProyectsSection id="projects"/>
+  <WorkExperience id="experience"/> 
+  <Achievements id="achievements"/>
+  <ContactMe id="contact-me"/>
+</template>
+

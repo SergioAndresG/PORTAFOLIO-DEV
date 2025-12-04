fae50f6efa958efcf2db516c455e57dd8e675419
SergioAndresG <140452759+SergioAndresG@users.noreply.github.com>
2025-12-04 16:35:43 -0500

    fix(): Se integra el componente en la pagina principal

--- portafolio/src/App.vue
+++ portafolio/src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router';
+import Animation from './components/Animation.vue';
 
 const route = useRouter()
 
 </script>
 
 <template>
+    <Animation />
     <router-view/>
 </template>
 
